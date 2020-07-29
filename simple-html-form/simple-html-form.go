7a369f27e0bb111d76e1826f5b7adae02a4da583
Ban Nguyen <ban.nguyen@asnet.com.vn>
2020-07-29 21:09:41 +0700

    Create simple form

---
+++ simple-html-form/simple-html-form.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gorilla/schema"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "8080"
+)
+
+type User struct {
+	Username string
+	Password string
+}
+
+func readForm(r *http.Request) *User {
+	r.ParseForm()
+
+	fmt.Println(r.PostForm)
+	user := new(User)
+	decoder := schema.NewDecoder()
+	decodeErr := decoder.Decode(user, r.PostForm)
+
+	if decodeErr != nil {
+		log.Printf("error mapping parsed form data to struct : ", decodeErr)
+	}
+
+	return user
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		parsedTemplate, _ := template.ParseFiles("./templates/login-form.html")
+		err := parsedTemplate.Execute(w, nil)
+
+		if err != nil {
+			log.Fatal("error parsing template : ", err)
+
+			return
+		}
+	} else {
+		user := readForm(r)
+		fmt.Fprintf(w, "Hello "+user.Username+"!")
+	}
+}
+
+func main() {
+	fmt.Println(filepath.Abs("./"))
+	http.HandleFunc("/", login)
+	err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, nil)
+
+	if err != nil {
+		log.Fatal("error starting http server : ", err)
+		return
+	}
+}
