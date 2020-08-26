5c21441f6d752e72516930bb7d2dee9512b126a7
Ban Nguyen <ban.nguyen.tla1@gmail.com>
2020-08-26 16:22:24 +0700

    Test nested module

--- module-demo/greetings/main.go
+++ module-demo/greetings/main.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"prefix-path-also-remote-url1.package.com/bantla/greetings/stringlib"
+	"prefix-path-also-remote-url1.package.com/bantla/greetings/stringlib/nested"
 )
 
 func main() {
 	fmt.Println(stringlib.ReverseRunes("Hello Go! - !oG ,olleH"))
+	fmt.Println(nested.ReverseFn("Hello Go! - !oG ,olleH"))
 	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
